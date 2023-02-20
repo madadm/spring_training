612e201ea2e22ce4d573824f6780e28eb27ccdda
oleksiikoval <lexi4.koval@gmail.com>
2023-02-20 14:43:23 -0500

    Case abstraction factory

---
+++ spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/stereotype_annotation/casefactory/DellCase.java
@@ -0,0 +1,15 @@
+package com.cydeo.stereotype_annotation.casefactory;
+
+public class DellCase extends Case{
+
+
+    public DellCase(String model, String manufacturer, String powerSupply) {
+        super(model, manufacturer, powerSupply);
+    }
+
+    @Override
+    public void pressPowerButton() {
+
+        System.out.println("Power button pressed");
+    }
+}
