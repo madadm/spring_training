395b9579cd88e57079daf444494fda31bb3804b7
oleksiikoval <lexi4.koval@gmail.com>
2023-02-20 14:53:58 -0500

    @ComponentScan for the configuration class

---
+++ spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/stereotype_annotation/config/PcConfig.java
@@ -0,0 +1,13 @@
+package com.cydeo.stereotype_annotation.config;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan
+public class PcConfig {
+
+
+}
