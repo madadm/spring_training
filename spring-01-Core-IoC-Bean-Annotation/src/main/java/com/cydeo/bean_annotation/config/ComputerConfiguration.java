dd5224416f355d334320fc1e71ca767772a951c9
oleksiikoval <lexi4.koval@gmail.com>
2023-02-20 14:26:17 -0500

    Initial commit

---
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/config/ComputerConfiguration.java
@@ -0,0 +1,34 @@
+package com.cydeo.bean_annotation.config;
+
+import com.cydeo.bean_annotation.loosely_coupled.casefactory.Case;
+import com.cydeo.bean_annotation.loosely_coupled.casefactory.DellCase;
+import com.cydeo.bean_annotation.loosely_coupled.monitorfactory.AcerMonitor;
+import com.cydeo.bean_annotation.loosely_coupled.monitorfactory.Monitor;
+import com.cydeo.bean_annotation.loosely_coupled.monitorfactory.SonyMonitor;
+import com.cydeo.bean_annotation.loosely_coupled.motherboardfactory.AsusMotherboard;
+import com.cydeo.bean_annotation.loosely_coupled.motherboardfactory.Motherboard;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ComputerConfiguration {
+
+    @Bean(name="sony")
+    public Monitor monitorSony(){
+        return new SonyMonitor("25 inch Beast", "Sony", 25);
+    }
+
+    @Bean
+    public Monitor monitorAcer(){
+        return new AcerMonitor("23 inch Beast", "Acer", 23);
+    }
+    @Bean
+    public Case dellCase(){
+        return new DellCase("220B", "Dell", "240");
+    }
+    @Bean
+    public Motherboard asusMotherBoard(){
+        return new AsusMotherboard("BJ-200", "Asus", 4, 6, "8");
+    }
+
+}
