81a769e77078051969e995cacdf9324526adb1ea
oleksiikoval <lexi4.koval@gmail.com>
2023-02-21 00:10:17 -0500

    Reading properties

--- spring-07-SpringBoot-Demo/src/main/java/com/cydeo/CydeoApplication.java
+++ spring-07-SpringBoot-Demo/src/main/java/com/cydeo/CydeoApplication.java
@@ -21,6 +21,8 @@ public class CydeoApplication {
 
         CommentService commentService =  container.getBean(CommentService.class);
         commentService.publishComment(comment);
+        commentService.printConfigData();
+        commentService.printDBConfigData();
     }
 
 }
