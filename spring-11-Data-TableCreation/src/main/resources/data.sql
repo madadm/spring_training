46b53f17a54eab3e585b1c7f3c35fdf9476e80db
oleksiikoval <lexi4.koval@gmail.com>
2023-04-26 09:11:35 -0400

    Tables creation

---
+++ spring-11-Data-TableCreation/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into EMPLOYEES(id,name)
+values(1,'Mike');
+
+
+insert into students(first_name, last_name, email)
+values ('Tom','Hanks', 'tom@cydeo.com');
+
+insert into students( first_name, last_name, email)
+values ( 'Mike','Smith', 'mike@cydeo.com');
