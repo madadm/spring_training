676d47b8342c56456a3a5b3ec6cf4825fd68142b
oleksiikoval <lexi4.koval@gmail.com>
2023-05-03 12:01:56 -0400

    DB

--- Spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/User.java
+++ Spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/User.java
@@ -1,14 +1,26 @@
 package com.cydeo.entity;
 
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
+@Table(name="user_account")
 public class User extends BaseEntity{
+
+    private String email;
+    private String password;
+
+    private String username;
+
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="account_details_id")
+    private Account account;
+
 
 
 }
